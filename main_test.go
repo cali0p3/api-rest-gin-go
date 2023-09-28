b98482fbd32d8e3198e630a7fa4cc96d0ca5588e
Cali0p3 <robertta.longoni@gmail.com>
2023-09-28 11:27:55 -0300

    Add tests

--- main_test.go
+++ main_test.go
@@ -9,14 +9,28 @@ import (
 
 	"api-go-gin/controllers"
 	"api-go-gin/database"
+	"api-go-gin/models"
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
+var ID int
+
 func SetupDasRotasDeTeste() *gin.Engine {
 	rotas := gin.Default()
 	return rotas
+}
+
+func CriaAlunoMock(){
+	aluno := models.Aluno{Nome: "Aluno Teste", CPF: "12345678901", RG: "123456789"}
+	database.DB.Create(&aluno)
+	ID = int(aluno.ID)
+}
+
+func DeletaAlunoMock() {
+	var aluno models.Aluno
+	database.DB.Delete(&aluno, ID)
 }
 
 func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
@@ -33,15 +47,27 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	fmt.Println(mockDaResposta)
 }
 
-func TestListandoTodosAlunos(t *testing.T) {
+func TestListandoTodosAlunosHandler(t *testing.T) {
 	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
 	r := SetupDasRotasDeTeste()
 	r.GET("/alunos", controllers.TodosAlunos)
 	req, _ := http.NewRequest("Get", "/alunos", nil)
 	resposta := httptest.NewRecorder()
 	r.ServeHTTP(resposta, req)
 	assert.Equal(t, http.StatusOK, resposta.Code)
-	fmt.Println(resposta.Body)
 }
 
+func TestaBuscaAlunoPorCPFHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeletaAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
+	req, _ := http.NewRequest("GET", "/alunos/cpf/12345678901", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+}
 
