7e983f13f68debafda58b92c4df0f7850c9a9116
Cali0p3 <robertta.longoni@gmail.com>
2023-09-28 10:49:40 -0300

    First commit

---
+++ main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"api-go-gin/controllers"
+	"api-go-gin/database"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func SetupDasRotasDeTeste() *gin.Engine {
+	rotas := gin.Default()
+	return rotas
+}
+
+func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
+	r := SetupDasRotasDeTeste()
+	r.GET("/:nome, controllers.Saudacoes")
+	req, _ := http.NewRequest("GET", "/Robertta", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code, "Valores deveriam ser iguais!")
+	mockDaResposta := `{"API diz":"E ai Robertta, Tudo beleza?"}`
+	respostaBody, _ := ioutil.ReadAll(resposta.Body)
+	assert.Equal(t, mockDaResposta, string(respostaBody))
+	fmt.Println(string(respostaBody))
+	fmt.Println(mockDaResposta)
+}
+
+func TestListandoTodosAlunos(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.TodosAlunos)
+	req, _ := http.NewRequest("Get", "/alunos", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+	fmt.Println(resposta.Body)
+}
